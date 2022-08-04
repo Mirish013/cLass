23fd46a568bc1d6efaa115b3a8343b2131eb7f1a
Mirish013 <109863440+Mirish013@users.noreply.github.com>
2022-08-04 18:38:12 +0400

    cLass 2

---
+++ cLass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;*/
+
+namespace cLass
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Box b = new Box();
+            /*b.SetUzunluq(15555);
+            Console.WriteLine(b.GetUzunluq());*/
+
+            b.EN = -30;
+            Console.WriteLine(b.EN);
+        }
+    }
+}
