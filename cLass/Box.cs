13e440a38d16ed555209cbc97c098b39b63b87e7
Mirish013 <109863440+Mirish013@users.noreply.github.com>
2022-08-04 19:21:49 +0400

    cLass2.1

---
+++ cLass/Box.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace cLass
+{
+    internal class Box 
+    {
+        //Field
+        private double uzunluq;
+        private double en;
+        private double hundurluk;
+        private double geribil= 0;
+        
+        //Constructor
+        public Box()
+        {
+            //uzunluq = 5;
+            Console.WriteLine("ASDFUCK");
+        }
+
+        public Box(double uzunluq, double en, double hundurluk)
+        {
+            this.uzunluq = uzunluq;
+            this.en = en;
+            this.hundurluk = hundurluk;
+        }
+
+        /*~Box()
+        {
+            Console.WriteLine("Sikdiroza");
+            Console.ReadLine();
+        }*/
+
+        /*public string GetUzunluq()
+        {
+            if (uzunluq>=1)
+            {
+                string a = Convert.ToString(uzunluq);
+                return a;
+            }
+            else
+            {
+                return geribil;
+            }
+        }
+        public void SetUzunluq(double uzun)
+        {
+            this.uzunluq = uzun;
+        }*/
+
+        public double EN
+        {
+            get
+            {
+                if (en>=1)
+                {
+                    return en;
+                }
+                else
+                {
+
+                    return geribil; 
+                } 
+            }
+
+            set
+            {
+                this.en = value;
+            }
+        }
+
+    }
+}
